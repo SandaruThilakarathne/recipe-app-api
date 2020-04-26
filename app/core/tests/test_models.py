e6e0f2c1fdb5e06e9122ac166e2af43f2eea33e8
Theesh <theekshana.s@ayra.com.au>
2020-04-26 11:11:03 +0530

    creating user and superuser

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
+
 class ModelTests(TestCase):
 
     def test_create_user_with_email_successfull(self):
@@ -14,3 +15,24 @@ class ModelTests(TestCase):
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """ test the email for the user normalized """
+        email = "test@TESTSfdas.com"
+        user = get_user_model().objects.create_user(email, "Test@pass123")
+
+        self.assertEqual(user.email, email.lower())
+
+    def test_new_user_invalid_email(self):
+        """Test creating user with no email raise error"""
+        password = "test123as"
+
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, password)
+
+    def test_create_new_superuser(self):
+        """Test creating a new superuser"""
+        user = get_user_model().objects.create_superuser('test@123.com',
+                                                         "Test@pass123")
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
